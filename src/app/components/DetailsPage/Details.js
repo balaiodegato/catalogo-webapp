3812d324f2b775cfd7f4d042c4fa7ecd945a68b0
Felipe Machado <462154+felipou@users.noreply.github.com>
2020-02-02 11:24:37 -0300

    Make remaining MainInfo fields editable

--- src/app/components/DetailsPage/Details.js
+++ src/app/components/DetailsPage/Details.js
@@ -5,12 +5,19 @@ import './Details.css';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Select from '@material-ui/core/Select';
 import EditIcon from '@material-ui/icons/Edit';
 import SaveAltIcon from '@material-ui/icons/SaveAlt';
 import { makeStyles } from '@material-ui/core/styles';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 
 import example_photo from '../../../assets/pet_photo_example.jpeg'
 import { yellow } from '@material-ui/core/colors';
+import moment from 'moment';
 
 function ProfilePhoto(props) {
   return <Box width={props.width} height={props.height} border={20} borderColor={yellow} borderRadius={20}>
@@ -20,10 +27,13 @@ function ProfilePhoto(props) {
 
 const STATE_DESCRIPTIONS = {
   "resident": "residente do abrigo",
+  "other_option1": "opção 1",
+  "other_option2": "opção 2",
 };
 
 const TEST_RESULT_STRINGS = {
   "negative": "Negativo",
+  "positive": "Positivo",
 }
 
 const useStyles = makeStyles(theme => ({
@@ -39,11 +49,43 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+function formatDate(dt) {
+  if (!dt) {
+    return '-'
+  }
+
+  return dt.format('DD/MM/YYYY')
+}
+
+function EditableDateField(props) {
+  const defaultValue = props.defaultValue ? props.defaultValue.toDate() : new Date()
+  const [selectedDate, handleDateChange] = useState(defaultValue)
+
+  const onValueChange = value => {
+    handleDateChange(value)
+    props.onChange(value)
+  }
+
+  return <KeyboardDatePicker
+    autoOk
+    disableToolbar
+    variant="inline"
+    format="DD/MM/YYYY"
+    margin="normal"
+    label={props.label}
+    value={selectedDate}
+    onChange={onValueChange}
+    KeyboardButtonProps={{
+      'aria-label': 'change date',
+    }}
+  />
+}
+
 function useEditMode(saveCallback) {
   const [editMode, setEditMode] = useState(false);
   const [editValues, setEditValues] = useState({});
   function onValueChange(name, event) {
-    const value = event.target.value;
+    const value = (event && event.target && event.target.value) ? event.target.value : event;
     setEditValues(editValues => ({
       ...editValues,
       [name]: value,
@@ -81,20 +123,86 @@ function MainInfo(props) {
         }
       </Box>
       <Box display="flex" flexDirection="row">
-        <Box fontSize="25px">({STATE_DESCRIPTIONS[pet.current_state]})</Box>
+        {editMode ?
+          <Select
+            native
+            variant="outlined"
+            autoWidth={true}
+            label="Situação"
+            defaultValue={pet.current_state}
+            onChange={e => onValueChange('state', e)}>
+            {Object.keys(STATE_DESCRIPTIONS).map(code =>
+              <option key={code} value={code}>{STATE_DESCRIPTIONS[code]}</option>)}
+          </Select>
+          : <Box fontSize="25px">({STATE_DESCRIPTIONS[pet.current_state]})</Box>
+        }
       </Box>
       <Box fontSize="20px" marginTop="20px" display="flex" flexDirection="row">
         <Box display="flex" flexDirection="column" justifyContent="flex-start" alignContent="flex-start">
-          <Box display="flex"><span><b>Idade:</b> {pet.age}</span></Box>
-          <Box display="flex" marginTop="10px"><span><b>Data do resgate:</b> {pet.rescue_date}</span></Box>
-          <Box display="flex" marginTop="10px"><span><b>Teste: </b> {TEST_RESULT_STRINGS[pet.test_result]}</span></Box>
+          {editMode ?
+            <TextField
+              type="number"
+              InputLabelProps={{shrink: true}}
+              onChange={e => onValueChange('age', e)}
+              label="Idade (meses)"
+              variant="outlined"
+              defaultValue={pet.age} />
+            : <Box display="flex"><span><b>Idade:</b> {pet.age} meses</span></Box>
+          }
+          {editMode ?
+            <EditableDateField
+              label="Data do resgate"
+              defaultValue={pet.rescue_date}
+              onChange={value => onValueChange('rescue_date', value)}
+            />
+            : <Box display="flex" marginTop="10px"><span><b>Data do resgate:</b> {formatDate(pet.rescue_date)}</span></Box>
+          }
+          {editMode ?
+            <Select
+              native
+              variant="outlined"
+              autoWidth={true}
+              label="Teste"
+              defaultValue={pet.test_result}
+              onChange={e => onValueChange('state', e)}>
+              {Object.keys(TEST_RESULT_STRINGS).map(code =>
+                <option key={code} value={code}>{TEST_RESULT_STRINGS[code]}</option>)}
+            </Select>
+            : <Box display="flex" marginTop="10px"><span><b>Teste: </b> {TEST_RESULT_STRINGS[pet.test_result]}</span></Box>
+          }
         </Box>
         <Box marginLeft="20px" display="flex" flexDirection="column" justifyContent="flex-start" alignContent="flex-start">
-          <Box display="flex"><span><b>Sexo:</b> {pet.gender}</span></Box>
-          <Box display="flex" marginTop="10px"><span><b>Data da adoção:</b> {pet.adoption_date}</span></Box>
-          <Box display="flex" marginTop="10px"><span><b>Castrado: </b> {
-            pet.castration_date ? `Sim (${pet.castration_date})` : "Não"
-          }</span></Box>
+          {editMode ?
+            <Select
+              native
+              variant="outlined"
+              autoWidth={true}
+              label="Teste"
+              defaultValue={pet.gender}
+              onChange={e => onValueChange('state', e)}>
+              <option key='F' value='F'>Fêmea</option>
+              <option key='M' value='M'>Macho</option>
+            </Select>
+            : <Box display="flex"><span><b>Sexo:</b> {pet.gender}</span></Box>
+          }
+          {editMode ?
+            <EditableDateField
+              label="Data da adoção"
+              defaultValue={pet.adoption_date}
+              onChange={value => onValueChange('adoption_date', value)}
+            />
+            : <Box display="flex" marginTop="10px"><span><b>Data da adoção:</b> {formatDate(pet.adoption_date)}</span></Box>
+          }
+          {editMode ?
+            <EditableDateField
+              label="Data da castração"
+              defaultValue={pet.castration_date}
+              onChange={value => onValueChange('castration_date', value)}
+            />
+            : <Box display="flex" marginTop="10px"><span><b>Castrado: </b> {
+                pet.castration_date ? `Sim (${formatDate(pet.castration_date)})` : "Não"
+              }</span></Box>
+          }
         </Box>
       </Box>
     </Box>
@@ -137,21 +245,23 @@ function Details(props) {
     name: "Renatinho",
     current_state: "resident",
     age: 12,
-    rescue_date: "2018-12-09",
+    rescue_date: moment("2018-12-09"),
     test_result: "negative",
     gender: "M",
     adoption_date: null,
-    castration_date: "2018-12-15",
+    castration_date: moment("2018-12-15"),
     rescue_info: "Lorem ipsum rescue",
     behaviour_info: "Lorem ipsum behaviour",
   };
 
   return (
     <Box padding="20px" display="flex" flexDirection="column" alignItems="center" justifyContent="center" bgcolor="#EEEEEE">
-      <Box width="1000px" display="flex" justifyContent="center">
-        <ProfilePhoto url={pet.photo_url} width="200px" height="200px"></ProfilePhoto>
-        <MainInfo pet={pet} onSave={onSave}></MainInfo>
-      </Box>
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <Box width="1000px" display="flex" justifyContent="center">
+          <ProfilePhoto url={pet.photo_url} width="200px" height="200px"></ProfilePhoto>
+          <MainInfo pet={pet} onSave={onSave}></MainInfo>
+        </Box>
+      </MuiPickersUtilsProvider>
       <InfoBox
         title="Informações comportamentais"
         text={pet.rescue_info}
