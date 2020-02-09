9c6374b44b17354843c4cd208af7d7b654475495
danieltorres31 <danieltorres31@outlook.com>
2020-02-09 17:56:57 -0300

    Configure initial routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Details from './DetailsPage/Details.js';
-import AnimalList from './AnimalList/AnimalList';
+import Details from './app/components/DetailsPage/Details.js';
+import AnimalList from './app/components/AnimalList/AnimalList';
+import Header from './app/components/Header'
+import { Route, Switch } from 'react-router-dom'
+import { Router } from 'react-router'
+import { createBrowserHistory } from 'history'
+
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-      <Details/>
-      <AnimalList />
-    </div>
+    <Router history={history}>
+      <div className="App">
+        <Header />
+      </div>
+
+      <Switch>
+        <Route exact path="/details">
+          <Details/>
+        </Route>
+        <Route exact path="/animals">
+          <AnimalList />
+        </Route>
+      </Switch>
+
+    </Router>
   )
 }
 
