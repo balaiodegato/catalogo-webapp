2f8e690e565bf95be1b86fbd8e9a9cfc29b04465
danieltorres31 <danieltorres31@outlook.com>
2020-02-26 11:34:20 -0300

    Refatora AnimalList para PetList

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Details from './app/components/DetailsPage/Details.js';
-import AnimalList from './app/components/AnimalList/AnimalList';
+import PetList from './app/components/PetList/PetList';
 import Header from './app/components/Header'
 import { Route, Switch } from 'react-router-dom'
 import { Router } from 'react-router'
@@ -14,12 +14,12 @@ function App() {
   return (
     <Router history={history}>
       <div className="App">
-        <Header />
+        {/* <Header /> */}
       </div>
 
       <Switch>
         <Route exact path="/">
-          <AnimalList />
+          <PetList />
         </Route>
         <Route exact path="/details/:id">
           <Details/>
