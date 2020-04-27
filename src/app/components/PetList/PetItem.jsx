4efd6b92fd8f67e78a417623a3443aa8d38df171
danieltorres31 <danieltorres31@outlook.com>
2020-04-27 18:20:13 -0300

    Ajusta classes css do status

--- src/app/components/PetList/PetItem.jsx
+++ src/app/components/PetList/PetItem.jsx
@@ -12,10 +12,10 @@ export function PetItem({ pet }) {
     const history = useHistory()
 
     const statusClass = {
-        'Adotado': [classes.statusIndicator, classes.statusIndicatorAdotado],
-        'Estrelinha': [classes.statusIndicator, classes.statusIndicatorEstrelinha],
-        'Residente': [classes.statusIndicator, classes.statusIndicatorResidente],
-        'Para adoção': [classes.statusIndicator, classes.statusIndicatorAdocao]
+        'Adotado': { ...classes.statusIndicator, ...classes.statusIndicatorAdotado },
+        'Estrelinha': { ...classes.statusIndicator, ...classes.statusIndicatorEstrelinha },
+        'Residente': { ...classes.statusIndicator, ...classes.statusIndicatorResidente },
+        'Para adoção': { ...classes.statusIndicator, ...classes.statusIndicatorAdocao }
     }
 
     const navigateToDetails = () => {
