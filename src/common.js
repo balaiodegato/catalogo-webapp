e39cf42cabfa7c30fd2b9fec735031cdf706aba2
danieltorres31 <danieltorres31@outlook.com>
2020-04-30 20:32:16 -0300

    Add novos valores no commons

--- src/common.js
+++ src/common.js
@@ -1,7 +1,21 @@
 
-module.exports.STATE_COLORS = {
+export const STATES = {
+  'star': 'star',
+  'available': 'available',
+  'adopted': 'adopted',
+  'resident': 'resident'
+}
+
+export const STATE_COLORS = {
   'star': '#f2a994',
   'available': '#a0db8e',
   'adopted': '#92a8d1',
   'resident': '#007239',
+}
+
+export const STATE_LABELS = {
+  'star': 'Estrelinha',
+  'available': 'Para Adoção',
+  'adopted': 'Adotado',
+  'resident': 'Residente',
 }
