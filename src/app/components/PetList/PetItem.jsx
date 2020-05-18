485bc3f198ea91cfa63e6fcdac3ac94e7ff31a47
danieltorres31 <danieltorres31@outlook.com>
2020-05-18 19:04:56 -0300

    Remove importacao nao usada

--- src/app/components/PetList/PetItem.jsx
+++ src/app/components/PetList/PetItem.jsx
@@ -1,4 +1,4 @@
-import React, { createRef, useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { Grid, Box, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom'
